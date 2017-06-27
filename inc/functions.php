47e44862970a8a5cc8e17cd0c53bb4d3da711ba5
Fernando Bevilacqua <dovyski@gmail.com>
2017-06-27 16:29:50 +0200

    Index aggregated task data using permutation hash

--- inc/functions.php
+++ inc/functions.php
@@ -11,11 +11,12 @@ function aggredateTaskInfos($theTaskJsonFiles) {
 
     foreach($theTaskJsonFiles as $aFile) {
         $aInfo = json_decode(file_get_contents($aFile), true);
+        $aPermutation = $aInfo['permutation'];
 
         // TODO: get progress and result data from log file
-        $aInfos[] = array(
+        $aInfos[$aPermutation] = array(
             'commit'        => $aInfo['hash'],
-            'permutation'   => $aInfo['permutation'],
+            'permutation'   => $aPermutation,
             'date'          => date('d-m-Y H:i:s', $aInfo['time']),
             'params'        => $aInfo['params'],
             'cmd'           => $aInfo['cmd'],
@@ -34,7 +35,7 @@ function findTasksInfos($theDataDir) {
 
     foreach($aTasks as $aItem) {
         $aPath = $theDataDir . DIRECTORY_SEPARATOR . $aItem;
-        
+
         if($aItem[0] != '.' && is_dir($aPath)) {
             $aFiles = glob($aPath . DIRECTORY_SEPARATOR . '*.json');
             $aData[$aItem] = aggredateTaskInfos($aFiles);
