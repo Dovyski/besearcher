545cb756299721890dfc346d5c7ad38f5e6388a9
Fernando Bevilacqua <dovyski@gmail.com>
2017-06-28 14:17:37 +0200

    Rename config file

--- www/inc/globals.php
+++ www/inc/globals.php
@@ -1,6 +1,6 @@
 <?php
 
-$aINI = parse_ini_file(dirname(__FILE__) . '/../config.ini');
+$aINI = @parse_ini_file(dirname(__FILE__) . '/../config.ini');
 
 if($aINI === false) {
 	die('There is a syntax error in config.ini or it does not exist.');
@@ -12,4 +12,5 @@ define('PATH_BESERCHER_INI_FILE', @$aINI['besearcher_ini_file']);
 require_once(dirname(__FILE__) . '/../../inc/functions.php');
 require_once(dirname(__FILE__) . '/view.php');
 require_once(dirname(__FILE__) . '/data.php');
+
 ?>
