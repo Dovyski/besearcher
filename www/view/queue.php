10698b58cd0690ea6c9a24a315602730643698db
Fernando Bevilacqua <dovyski@gmail.com>
2017-09-05 15:53:03 +0200

    Fix wrong page title

--- www/view/queue.php
+++ www/view/queue.php
@@ -7,7 +7,7 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Control <i class="fa fa-question-circle" title="This page shows the internal data that Besearcher is using to process the tasks. You can change or cancel tasks, for instance."></i></h1>
+            <h1 class="page-header">Queue <i class="fa fa-question-circle" title="This page shows the internal data that Besearcher is using to process the tasks. You can change or cancel tasks, for instance."></i></h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
--- www/view/results.php
+++ www/view/results.php
@@ -8,7 +8,7 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Tasks</h1>
+            <h1 class="page-header">Results</h1>
         </div>
         <!-- /.col-lg-12 -->
     </div>
@@ -20,7 +20,7 @@
                 <thead>
                     <tr>
                         <th><i class="fa fa-info-circle"></i></th>
-                        <th>Commit-permutation</th>
+                        <th>Hash-permutation</th>
                         <th>Creation</th>
                         <th>Start</th>
                         <th>Params</th>
