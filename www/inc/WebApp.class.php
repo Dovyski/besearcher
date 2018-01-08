51c6390d7090746dc6157bcc57c9922a06e4edd4
Fernando Bevilacqua <dovyski@gmail.com>
2018-01-08 09:07:35 +0100

    Web dashboard is able to load more than one entry under "besearcher_ini_file"

--- www/inc/WebApp.class.php
+++ www/inc/WebApp.class.php
@@ -6,21 +6,14 @@ class WebApp {
 	private static $mInstance;
 	private static $mUsers;
 	private static $mINI;
+	private static $mActiveBesearcherINIPath;
 
 	public static function init($theINIPath) {
-		self::$mINI = @parse_ini_file($theINIPath);
-
-		if(self::$mINI === false) {
-			throw new \Exception('There is a syntax error in the web dashboard configuration file or it does not exist. Path to the file is: <code>'.$theINIPath.'</code>');
-		}
-
-		if(!isset(self::$mINI['besearcher_ini_file'])) {
-			throw new \Exception('Unable to find the <code>besearcher_ini_file</code> directive in the web dashboard configuration file <code>'.$theINIPath.'</code>. Please check the content of this file is correct.');
-		}
+		self::loadWebAppINI($theINIPath);
 
 		try {
 			self::$mInstance = new App();
-			self::$mInstance->init(self::$mINI['besearcher_ini_file'], '', true);
+			self::$mInstance->init(self::getBesearcherINIPath(), '', true);
 			self::$mUsers = new Users(self::$mInstance->getDb());
 
 		} catch (\Exception $e) {
@@ -38,6 +31,40 @@ class WebApp {
 
 	public static function config($theKey, $theDefault = null) {
 		return isset(self::$mINI[$theKey]) ? self::$mINI[$theKey] : $theDefault;
+	}
+
+	private static function loadWebAppINI($theINIPath) {
+		self::$mINI = @parse_ini_file($theINIPath);
+
+		if(self::$mINI === false) {
+			throw new \Exception('There is a syntax error in the web dashboard configuration file or it does not exist. Path to the file is: <code>'.$theINIPath.'</code>');
+		}
+
+		if(!isset(self::$mINI['besearcher_ini_file'])) {
+			throw new \Exception('Unable to find the <code>besearcher_ini_file</code> directive in the web dashboard configuration file <code>'.$theINIPath.'</code>. Please check the content of this file is correct.');
+		}
+
+		self::initBesearcherINIPaths();
+	}
+
+	private static function initBesearcherINIPaths() {
+		// besearcher_ini_file must be an array of possible besearcher INI files.
+		// If a single entry was informed in the web app INI, we convert it to
+		// an array (containing a single element)
+		if(!is_array(self::$mINI['besearcher_ini_file'])) {
+			self::$mINI['besearcher_ini_file'] = array(self::$mINI['besearcher_ini_file']);
+		}
+
+		// By default, the first entry in the array is the active besearcher INI file.
+		self::$mActiveBesearcherINIPath = 0;
+	}
+
+	private static function getBesearcherINIPath() {
+		return self::$mINI['besearcher_ini_file'][self::$mActiveBesearcherINIPath];
+	}
+
+	private static function findBesearcherINIPaths() {
+		return self::$mINI['besearcher_ini_file'];
 	}
 }
 
